d9471cc7d6a63bc107b887ee96da8e23d0a81504
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-12-02 22:35:58 -0300

    Create GwInfo message

---
+++ pkg/mqttsn/gwinfo.go
@@ -0,0 +1,27 @@
+package mqttsn
+
+import "errors"
+
+// GwInfoMessage represents the contents of a MQTT-SN GWINFO message.
+type GwInfoMessage struct {
+	GatewayID      uint8
+	GatewayAddress []byte
+}
+
+// Marshal converts a message to its binary form.
+func (m *GwInfoMessage) Marshal() []byte {
+	body := make([]byte, 1, 1+len(m.GatewayAddress))
+	body[0] = m.GatewayID
+	return append(body, m.GatewayAddress...)
+}
+
+// Unmarshal parses a binary buffer into a message.
+func (m *GwInfoMessage) Unmarshal(body []byte) error {
+	if len(body) < 1 {
+		return errors.New("message: body has invalid size")
+	}
+
+	m.GatewayID = body[0]
+	m.GatewayAddress = body[1:]
+	return nil
+}
