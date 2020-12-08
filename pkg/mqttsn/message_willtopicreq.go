1ddde98b541f3789a2d0ea4110ced0cb6b64c58e
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-12-08 17:39:20 -0300

    Replace WillTopicReqMessage by alias to emptyMessage

--- pkg/mqttsn/message_willtopicreq.go
+++ pkg/mqttsn/message_willtopicreq.go
@@ -1,19 +1,4 @@
 package mqttsn
 
-import "fmt"
-
 // WillTopicReqMessage represents the contents of a MQTT-SN WILLTOPICREQ message.
-type WillTopicReqMessage struct{}
-
-// MarshalBinary implements encoding.BinaryMarshaler.MarshalBinary.
-func (m *WillTopicReqMessage) MarshalBinary() ([]byte, error) {
-	return []byte{}, nil
-}
-
-// UnmarshalBinary implements encoding.BinaryUnmarshaler.UnmarshalBinary.
-func (m *WillTopicReqMessage) UnmarshalBinary(body []byte) error {
-	if len(body) != 0 {
-		return fmt.Errorf("mqttsn: invalid body length (%v)", len(body))
-	}
-	return nil
-}
+type WillTopicReqMessage = emptyMessage
