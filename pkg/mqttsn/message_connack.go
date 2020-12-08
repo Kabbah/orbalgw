feb0e02cdb53e5861b19a70874527b352a34f1c6
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2020-12-08 17:38:23 -0300

    Replace ConnAckMessage by alias to returnCodeOnlyMessage

--- pkg/mqttsn/message_connack.go
+++ pkg/mqttsn/message_connack.go
@@ -1,25 +1,4 @@
 package mqttsn
 
-import "fmt"
-
 // ConnAckMessage represents the contents of a MQTT-SN CONNACK message.
-type ConnAckMessage struct {
-	ReturnCode ReturnCode
-}
-
-// MarshalBinary implements encoding.BinaryMarshaler.MarshalBinary.
-func (m *ConnAckMessage) MarshalBinary() ([]byte, error) {
-	body := make([]byte, 1)
-	body[0] = uint8(m.ReturnCode)
-	return body, nil
-}
-
-// UnmarshalBinary implements encoding.BinaryUnmarshaler.UnmarshalBinary.
-func (m *ConnAckMessage) UnmarshalBinary(body []byte) error {
-	if len(body) != 1 {
-		return fmt.Errorf("mqttsn: invalid body length (%v)", len(body))
-	}
-
-	m.ReturnCode = ReturnCode(body[0])
-	return nil
-}
+type ConnAckMessage = returnCodeOnlyMessage
